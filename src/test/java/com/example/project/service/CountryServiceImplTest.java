8f1f21e6cbe34d3eb80d043f027d50b1e4d5bbf8
rohanzemoso <rohan.jadhao@zemosolabs.com>
2024-09-03 13:17:22 +0530

    :memo: chores: update countryServiceImplTest to add unit tests to handle null id

--- src/test/java/com/example/project/service/CountryServiceImplTest.java
+++ src/test/java/com/example/project/service/CountryServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.project.service;
 
 import com.example.project.dao.CountryDao;
 import com.example.project.model.Country;
+import javax.persistence.EntityNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -74,4 +76,49 @@ public class CountryServiceImplTest {
         countryService.deleteCountry(countryId);
         verify(countryDao, times(1)).deleteCountry(countryId);
     }
+
+    @Test
+    public void testDeleteCountry_whenCountryIdNotPresent_shouldThrowException() {
+        int countryId = 1;
+        doThrow(new EntityNotFoundException("Country not found with id: " + countryId))
+                .when(countryDao).deleteCountry(countryId);
+        try {
+            countryService.deleteCountry(countryId);
+            fail("expected EntityNotFoundException to be thrown");
+        } catch (EntityNotFoundException e) {
+            assertEquals("Country not found with id: " + countryId, e.getMessage());
+        }
+        verify(countryDao, times(1)).deleteCountry(countryId);
+    }
+
+    @Test
+    public void testDeleteCountry_InvalidId() {
+        int invalidId = 0;
+        doThrow(new EntityNotFoundException("Country not found with id: " + invalidId))
+                .when(countryDao).deleteCountry(invalidId);
+        try {
+            countryService.deleteCountry(invalidId);
+            fail("expected EntityNotFoundException to be thrown");
+        } catch (EntityNotFoundException e) {
+            assertEquals("Country not found with id: " + invalidId, e.getMessage());
+        }
+        verify(countryDao, times(1)).deleteCountry(invalidId);
+    }
+
+    @Test
+    public void testGetCountryById_whenCountryIdNotPresent_shouldThrowException() {
+        int countryId = 1;
+        when(countryDao.getCountryById(countryId))
+                .thenThrow(new EntityNotFoundException("Country not found with id: " + countryId));
+
+        try {
+            countryService.getCountryById(countryId);
+            fail("expected EntityNotFoundException to be thrown");
+        } catch (EntityNotFoundException e) {
+            assertEquals("Country not found with id: " + countryId, e.getMessage());
+        }
+
+        verify(countryDao, times(1)).getCountryById(countryId);
+    }
+
 }
